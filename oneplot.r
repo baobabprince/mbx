69ca1944a195d22a12aad33f1762944ab288ae5f
baobabprince <46530629+baobabprince@users.noreply.github.com>
2021-05-12 16:41:54 +0300

    filter out guanine and adenosine outliers

--- oneplot.r
+++ oneplot.r
@@ -14,7 +14,9 @@ all <-
   pivot_longer(contains("_"), names_to = "SampleID", values_to = "value") %>% 
   left_join(groups) %>% 
   left_join(metadata, by = "SampleID") %>% 
-  filter(Compound %in% groups$Compound) 
+  filter(Compound %in% groups$Compound) %>%
+  filter(!Compound == "Adenosine" && value >2e7) %>%
+  filter(!Compound == "Guanine" && value >3e6)
 
 pl <- 
   ggplot(all, aes(x = Disease_Status, y = value, fill = Disease_Status)) + 
@@ -25,4 +27,4 @@ pl <-
   theme(axis.text.x = element_blank(), axis.ticks = element_blank()
         , strip.text = element_text(size = 6), strip.background = element_blank())
 
-ggsave(paste0("all_with_fasting", "_metabolites.png"), pl, height = 20, width = 30)
+ggsave(paste0("all_with_fasting", "_metabolites.png"), pl, height = 15, width = 20)
