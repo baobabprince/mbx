4847f80041f006a2859553c72b427681766ee909
Rotem Hadar <rotem.hadar@sheba.health.gov.il>
2021-05-13 15:22:47 +0000

    summarize for each pn_ID. improve plotting

--- oneplot.r
+++ oneplot.r
@@ -14,15 +14,15 @@ all <-
   pivot_longer(contains("_"), names_to = "SampleID", values_to = "value") %>% 
   left_join(groups) %>% 
   left_join(metadata, by = "SampleID") %>% 
-  filter(Compound %in% groups$Compound) 
+  filter(Compound %in% groups$Compound) %>% group_by(Compound, pn_ID) %>% summarize(value = mean(value), Disease_Status = first(Disease_Status))
 
 pl <- 
-  ggplot(all, aes(x = Disease_Status, y = value, fill = Disease_Status)) + 
-  geom_boxplot() + 
-  # geom_jitter(alpha = .3)+
+  ggplot(all, aes(x = Disease_Status, y = value, fill = Disease_Status, color = Disease_Status)) + 
+  geom_boxplot(outlier.size = .1, alpha = .6) + 
+  geom_jitter(alpha = .6)+
   facet_wrap(~Compound, scales = "free_y") +
   theme_classic()+
   theme(axis.text.x = element_blank(), axis.ticks = element_blank()
-        , strip.text = element_text(size = 6), strip.background = element_blank())
+        , strip.text = element_text(size = 18), strip.background = element_blank(), axis.text = element_text(size = 14))
 
 ggsave(paste0("all_with_fasting", "_metabolites.png"), pl, height = 20, width = 30)
