17e779639d9e907b254f0845b5e79e6ad598f3f3
Rotem <rotem.hadar@sheba.health.gov.il>
2022-09-14 16:12:54 +0300

    cluster features

--- Serum_Random_Forest_first_nf_alloptions2-17Jul2022.R
+++ Serum_Random_Forest_first_nf_alloptions2-17Jul2022.R
@@ -1,62 +1,63 @@
-library (tidyverse)
+library(tidyverse)
 library(dplyr)
 library(randomForest)
 library(AUC)
 library(ggplot2)
-setwd('~/Dropbox/Metabolomics')
+# setwd('~/Dropbox/Metabolomics')
 
 cv <- function(x){sd(x)/mean(x)}
 
-data_source <- 'MZPos_N1_filtered'   #'_MZPos-N1_biom2'   #'_MZNeg-N1_biom2'    #'MZM10_N2_filtered'
+data_source <- 'MZPos_N1_filtered'   #'_MZPos-N1_biom2'   #'_MZNeg-N1_biom2'    #'MZM10_N2_filtered' # nolint
+data_source <-"MZM10_N2_filtered"
 samples_type <- 'Serum'
-#samples_type <- 'Stool'
+# samples_type <- 'Stool'
 check_val = 'IIRN_Flare'
-na_str = c('no_data','_','NA','unknown', 'other','na')
+na_str = c('no_data','_','NA','unknown', 'other','na') # nolint
 
 map_file <- paste0(samples_type,'_metadata.txt')
-data_file <- paste0(samples_type,'_Data_',data_source,".tsv")  ## For untargeted files
+data_file <- paste0(samples_type,'_Data_',data_source,"_calour.tsv")  ## For untargeted files
 # data_file <- paste0(samples_type,'_Data_',data_source,"_calour.tsv")  ## For targeted files
 
-map0 <- paste0('./Final_datasets/',map_file) %>% read_tsv()
+map0 <- paste0(map_file) |> read_tsv()
 map0$SampleID <- make.names(map0$SampleID)
 
-data1 <- paste0('./Final_datasets/',data_file) %>% read.table(sep="\t", na.strings = na_str, row.names = 1) %>% t() %>% as.data.frame()
+data1 <- paste0(data_file) |> read.table(sep="\t", na.strings = na_str, row.names = 1) |> t() |> as.data.frame()
 #colnames(data1)[1]  <-  'SampleID'
-data1 <- data1 %>% rename(SampleID = CompoundID)
+#data1 <- data1 |> rename(SampleID = CompoundID)
 data1$SampleID <- make.names(data1$SampleID)
-data1 <- data1 %>% mutate_at(c(colnames(data1[-1])), as.numeric) #change the types of the columns (with the peak area values) from character to numeric.
+data1 <- data1 |> mutate_at(c(colnames(data1[-1])), as.numeric) #change the types of the columns (with the peak area values) from character to numeric.
 
-map0 <- map0 %>% filter(SampleID != 'X013_SMC_IIRN_p8s1')  #'X015_SMC_IIRN_p8s3'
-map1 <- map0 %>% inner_join(data1 %>% select('SampleID'))
+map0 <- map0 |> filter(SampleID != 'X013_SMC_IIRN_p8s1')  #'X015_SMC_IIRN_p8s3'
+map1 <- map0 |> inner_join(data1 |> select('SampleID'))
 
-map1 <- map1 %>% filter(Cohort == 'IIRN') %>% filter(Disease_Status == 'CD_Remission')
+map1 <- map1 |> filter(Cohort == 'IIRN') |> filter(Disease_Status == 'CD_Remission')
 
-map1_nf <- map1 %>% filter(IIRN_Flare == 'never_flared')
-map1_nf2 <- map1 %>% filter(IIRN_Flare == 'never_flared') %>% filter(Visit == 1)
-missing_pn <- setdiff(map1_nf$pn_ID%>% unique(), map1_nf2$pn_ID %>% unique())
+map1_nf <- map1 |> filter(IIRN_Flare == 'never_flared')
+map1_nf2 <- map1 |> filter(IIRN_Flare == 'never_flared') |> filter(Visit == 1)
+missing_pn <- setdiff(map1_nf$pn_ID|> unique(), map1_nf2$pn_ID |> unique())
 
-map1_nf_misspn <- map1_nf %>% filter(map1_nf$pn_ID %in% missing_pn) %>% filter(Visit == 3)
-map1_nf2 <- map1_nf2 %>% rbind(map1_nf_misspn) ## Should be 14 samples(14 subjects)
+map1_nf_misspn <- map1_nf |> filter(map1_nf$pn_ID %in% missing_pn) |> filter(Visit == 3)
+map1_nf2 <- map1_nf2 |> rbind(map1_nf_misspn) ## Should be 14 samples(14 subjects)
 
-map1_f <- map1 %>% filter(IIRN_Flare == 'flared')
-map1_f2 <- map1 %>% filter(IIRN_Flare == 'flared') %>% filter(Sample_Flare == 'Pre-Flare_3')
+map1_f <- map1 |> filter(IIRN_Flare == 'flared')
+map1_f2 <- map1 |> filter(IIRN_Flare == 'flared') |> filter(Sample_Flare == 'Pre-Flare_3')
 
-missing_pnf <- setdiff(map1_f$pn_ID%>% unique(), map1_f2$pn_ID %>% unique())
-map1_f_misspn <- map1_f %>% filter(map1_f$pn_ID %in% missing_pnf) %>% filter(Sample_Flare == 'Pre-Flare_6')
-map1_f2 <- map1_f2 %>% rbind(map1_f_misspn) ## Should be 11 samples(11 subjects)
-map1_f2$pn_ID %>% n_distinct()
+missing_pnf <- setdiff(map1_f$pn_ID|> unique(), map1_f2$pn_ID |> unique())
+map1_f_misspn <- map1_f |> filter(map1_f$pn_ID %in% missing_pnf) |> filter(Sample_Flare == 'Pre-Flare_6')
+map1_f2 <- map1_f2 |> rbind(map1_f_misspn) ## Should be 11 samples(11 subjects)
+map1_f2$pn_ID |> n_distinct()
 
-map3 <- map1_nf2 %>% rbind(map1_f2)
-map3$pn_ID %>% n_distinct() ## Should be total 25 samples(25 subjects)
+map3 <- map1_nf2 |> rbind(map1_f2)
+map3$pn_ID |> n_distinct() ## Should be total 25 samples(25 subjects)
 
 map3  <- droplevels(map3)
 
-data2 <- data1 %>% inner_join(map3 %>% select('SampleID'))
-#data2$IIRN_Flare <- data2$IIRN_Flare %>% as.factor()
+data2 <- data1 |> inner_join(map3 |> select('SampleID'))
+#data2$IIRN_Flare <- data2$IIRN_Flare |> as.factor()
 setdiff(map3$SampleID, data2$SampleID)
 
 # pl1 <- 
-#   data2 %>% select(-SampleID) %>% apply(2, cv) %>% data.frame() %>% 
+#   data2 |> select(-SampleID) |> apply(2, cv) |> data.frame() |> 
 #   ggplot(aes(.)) + geom_histogram(color="black", fill = "navy", binwidth = 0.25) + 
 #   ggtitle("Serum compounds SD/mean distribution") +
 #   #scale_x_continuous(breaks = seq(0, 10, 0.5)) +
@@ -68,21 +69,21 @@ setdiff(map3$SampleID, data2$SampleID)
 # pl1
 # ggsave(plot = pl1, filename = "Serum_compounds_IIRN1_cv1.png")
 # 
-data2t <- data2 %>% column_to_rownames("SampleID") %>% t() %>% as.data.frame()
-data2t <- data2t %>% mutate(sd1mean = data2t %>% apply(1, cv))
+data2t <- data2 |> column_to_rownames("SampleID") |> t() |> as.data.frame()
+data2t <- data2t |> mutate(sd1mean = data2t |> apply(1, cv))
 #q25 <- quantile(data$sd1mean)[2]
 
 top75 <- slice_max(data2t, prop=.5, order_by=sd1mean)
-##top75 %>% select(-sd1mean) %>% write_delim('Serum_IIRN1_top75compounds.tsv', delim = '\t')
+##top75 |> select(-sd1mean) |> write_delim('Serum_IIRN1_top75compounds.tsv', delim = '\t')
 
-# data3 <- top75 %>% select(-sd1mean) %>% t() %>% as.data.frame()
-# data3 <- data3 %>% mutate(SampleID = rownames(data3) ) %>% relocate(SampleID, .before = 1) %>% inner_join(map3 %>% select('SampleID','IIRN_Flare'))
+# data3 <- top75 |> select(-sd1mean) |> t() |> as.data.frame()
+# data3 <- data3 |> mutate(SampleID = rownames(data3) ) |> relocate(SampleID, .before = 1) |> inner_join(map3 |> select('SampleID','IIRN_Flare'))
 
-data3 <- data2 %>% inner_join(map3 %>% select('SampleID','IIRN_Flare'))
-data3$IIRN_Flare <- data3$IIRN_Flare %>% as.factor()
+data3 <- data2 |> inner_join(map3 |> select('SampleID','IIRN_Flare'))
+data3$IIRN_Flare <- data3$IIRN_Flare |> as.factor()
 
 # pl2 <- 
-#   data3 %>% select(-SampleID,-IIRN_Flare) %>% apply(2, cv) %>% data.frame() %>% 
+#   data3 |> select(-SampleID,-IIRN_Flare) |> apply(2, cv) |> data.frame() |> 
 #   ggplot(aes(.)) + geom_histogram(color="black", fill = "navy", binwidth = 0.25) + 
 #   ggtitle("Serum compounds SD/mean distribution") +
 #   #scale_x_continuous(breaks = seq(0, 10, 0.5)) +
@@ -96,14 +97,14 @@ data3$IIRN_Flare <- data3$IIRN_Flare %>% as.factor()
 
 df <- data.frame(1:100, row.names = 1:100)
 colnames(df) <- 'auc'
-df2 <- data.frame(feature = colnames(data3 %>% select(-SampleID,-IIRN_Flare)))
+df2 <- data.frame(feature = colnames(data3 |> select(-SampleID,-IIRN_Flare)))
 
 #set.seed(4)
 
 for (i in 1:100) {
   output.forest <- randomForest(y=data3$IIRN_Flare,
                                 ntree  = 1000,
-                                x = data3 %>% select(-SampleID, -IIRN_Flare)
+                                x = data3 |> select(-SampleID, -IIRN_Flare)
                                 , na.action = na.omit)
   res = roc(output.forest$votes[,1],factor(1 * (output.forest$y==levels(output.forest$y)[1] )))
   #print( auc(res,min = 0, max = 1) )
@@ -111,7 +112,7 @@ for (i in 1:100) {
   imp = as.data.frame(importance(output.forest,type = 2))
   imp = data.frame( feature = rownames(imp), MeanDecreaseGini = imp$MeanDecreaseGini)
   colnames(imp)[-1] <- paste0('MeanDecreaseGini_',as.character(i))
-  df2 <- df2 %>% full_join(imp)
+  df2 <- df2 |> full_join(imp)
 }
 
 print(paste0('Mean = ', mean(df$auc)))
@@ -119,14 +120,14 @@ print(paste0('SD = ',sd(df$auc)))
 #print(paste0('Median = ',median(df$auc)))
 #print(paste0('IQR = ',IQR(df$auc)))
 
-# df2t <- df2 %>% t() %>% as.data.frame()
+# df2t <- df2 |> t() |> as.data.frame()
 # colnames(df2t) <- df2$feature
 # df2t <- df2t[-1,]
 
-df2 <- df2 %>% mutate(impmean = df2 %>% select(-feature) %>% apply(1,mean))
+df2 <- df2 |> mutate(impmean = df2 |> select(-feature) |> apply(1,mean))
 df2 <- df2[rev(order(df2$impmean)), ]
 
-df3 <- df2[1:15,c('feature','impmean')] %>% as.data.frame()
+df3 <- df2[1:15,c('feature','impmean')] |> as.data.frame()
 
 varImpPlot(output.forest,  sort = T, n.var=15, main="Top 15 - Variable Importance",cex=0.6)
 
--- oneplot.r
+++ oneplot.r
@@ -4,10 +4,11 @@ data <- read_csv("Serum1_Data_TF.csv")
 data <- read_csv("TF-N1_206s.csv")
 
 data %<>% select(-contains("149"), -contains("150"), -contains("151"))
-metadata <- read_csv("Serum1_Metadata.csv")
+metadata <- read_csv("../Serum1_Metadata.csv")
 groups <- read_csv("metabolites groups.csv")
 
 names(groups)[2] <- "Compound"
+data |> View()
 
 all <- 
   data %>% 
@@ -27,5 +28,5 @@ pl <-
   theme_classic()+
   theme(axis.text.x = element_blank(), axis.ticks = element_blank()
         , strip.text = element_text(size = 18), strip.background = element_blank(), axis.text = element_text(size = 14))
-
+pl
 ggsave(paste0("all_with_fasting", "_metabolites.png"), pl, height = 15, width = 20)
